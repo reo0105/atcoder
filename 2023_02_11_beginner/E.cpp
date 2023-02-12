851ac2336bc3c7757ab7ec0502c007aaf5c7b56b
reo0105 <skreo0105@gmail.com>
2023-02-13 00:00:12 +0900

    2023/2/13_E

--- 2023_02_11_beginner/E.cpp
+++ 2023_02_11_beginner/E.cpp
@@ -4,21 +4,6 @@ using namespace std;
 typedef pair<int, bool> pb;
 typedef pair<int, int> pi;
 
-// vector<pi> dist(2005);
-// void dfs(int from)
-// {
-//     dist_d.at(from).first = 0;
-
-//     for (int next : G.at(from)) {
-//         if (dist_d.at(next) != -1) continue;
-//         t++;
-//         dfs(next);
-//     }
-
-//     t++;
-//     dist_f.at(from) = t;
-
-// }
 
 int main()
 {
@@ -31,46 +16,37 @@ int main()
         cin >> n >> m;
         for (int j = 1; j <= n; j++) cin >> c.at(j);
 
-        vector<vector<pb>> G(2005);
+        vector<vector<int>> G(2005);
         for (int j = 0; j < m; j++) {
             cin >> u >> v;
-            G.at(u).push_back(make_pair(v, true));
-            G.at(v).push_back(make_pair(u, true));
+            G.at(u).push_back(v);
+            G.at(v).push_back(u);
         }
 
-        // int ans = -1;
-        // ans = dfs(1);
-
-        int cnt = (c.at(1) == 0) ? -1 : 1;
-        vector<vector<pi>> dist(2005);
+        vector<vector<int>> dist(2005, vector<int>(2, -1));
         queue<tuple<int, int, int>> que;
-        dist.at(1).push_back(make_pair(0, cnt));
-        que.push(make_tuple(1, 0, cnt));
+        
+        dist.at(1).at(0) = 0;
+        dist.at(n).at(1) = 0;
+        que.push(make_tuple(1, n, 0));
+        
         int ans = -1;
-        cnt = 0;
-        // int fu = -1, fv = -1;
+        set<pi> done;
         while(!que.empty()) {
-            tuple tup = que.front();
+            tuple<int, int, int> tmp = que.front();
+            int takahasi = get<0>(tmp);
+            int aoki = get<1>(tmp);
+            int cnt = get<2>(tmp);
             que.pop();
-            int tu = get<0>(tup);
-            int dist = get<1>(tup);
-            int cost = get<2>(tup);
 
-            for (auto &tn : G.at(tu)) {
-                int tv = tn.first;
-                int ok = tn.second;
-                // cout << fu << " " << tu << " " << fv << " " << tv << endl;
-                if (cnt > m*1000) goto end;
-                
-                cnt++;
-                // tn.second = false;
-                // tn.second = false;
-                // cout << tu << " " << tv << " " << dist+1 << " " << cost << endl;
-                
-                if (c.at(tv) == 0) que.push(make_tuple(tv, dist+1, cost-1));
-                else que.push(make_tuple(tv, dist+1, cost+1));
-
-                if (tv == n && ((cost == 1 && c.at(tv) == 0) || (cost == -1 && c.at(tv) == 1))) { ans = dist + 1; goto end; }
+            for (int tn : G.at(takahasi)) {
+                for (int an : G.at(aoki)) {
+                    if (c.at(tn) != c.at(an) && !done.count(make_pair(tn, an))) {
+                        que.push(make_tuple(tn, an, cnt+1));
+                        done.insert(make_pair(tn, an));
+                        if (tn == n && an == 1) {ans = cnt + 1; goto end;}
+                    }
+                }
             }
         }
 end:
